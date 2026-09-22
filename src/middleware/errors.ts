eec0130b2106ee949ab883cc7d4afc11443edd58
ntombizodwantlahla53 <ntombizodwantlahla53@gmail.com>
2026-09-22 08:39:05 +0200

    updated middleware errorFile

--- src/middleware/errors.ts
+++ src/middleware/errors.ts
@@ -1,8 +1,21 @@
-import { Request,Response, NextFunction } from "express";
+import { Request, Response, NextFunction, ErrorRequestHandler } from "express";
 
 export const notFounderHandler = (req:Request, res:Response, next : NextFunction) =>{
     res.status(404).json({
 error : "Not Found",
 message: `The request URL ${req.originalUrl} was not found on this server.`
+})
+}
+
+export const wrongMethod = (req:Request, res:Response, next : NextFunction) =>{
+    res.status(405).json({
+error : "Not Found",
+})
+}
+
+export const errorHandler: ErrorRequestHandler = (error, req, res, next) =>{
+    const status = error.status || 500;
+    res.status(status).json({
+error : error.message ||  "Server error",
 })
 }
